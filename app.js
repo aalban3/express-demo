aacb1b2e81dd3e632837ca220e35d3f504f667ba
Alan Alban <Alan@macbook-pro-3.myfiosgateway.com>
2020-11-15 18:38:37 -0500

    uploading project

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+app.set("view engine","ejs")
+
+app.get(["/",'/home'], function(req,res){
+    res.render("index");
+})
+
+app.listen(3000,function(){
+    console.log("serving application on port 3000!");
+})
